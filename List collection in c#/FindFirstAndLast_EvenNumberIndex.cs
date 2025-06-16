0cd701f8f6cd3b0216549566602f396a1150f5a3
ganeshsambaru <ganeshsambaru@gmail.com>
2025-06-16 18:10:54 +0530

    c# collections

---
+++ List collection in c#/FindFirstAndLast_EvenNumberIndex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_collection_in_c_
+{
+    internal class FindFirstAndLast_EvenNumberIndex
+    {
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                List<int> numbers = new List<int> { 5, 7, 12, 19, 24, 31, 42, 9,31, 13 };
+
+                foreach(int number in numbers)
+                {
+                    Console.WriteLine(number);
+                }
+
+                Console.WriteLine("enter number");
+
+                int num = int.Parse(Console.ReadLine());
+                int firstindex = numbers.FindIndex(number => number == num);
+
+                Console.WriteLine($"Index of {num} is {firstindex}");
+
+                int lastindex = numbers.FindLastIndex(number => number == num);
+
+                Console.WriteLine($"Last Index of {num} is {lastindex}");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ List collection in c#/FindLastIndex_RemoveAll_AppleTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_collection_in_c_
+{
+    internal class FindLastIndex_RemoveAll_AppleTask
+    {
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                List<string> fruits = new List<string>
+                {
+                    "Mango", "Apple", "Banana", "Apple", "Guava", "Apple"
+                };
+                int lastindex = fruits.FindLastIndex(fruit => fruit =="Apple");
+
+                Console.WriteLine($"Last index of 'Apple': {lastindex}");
+
+                fruits.RemoveAll(fruit => fruit == "Apple");
+
+                foreach(string fruit in fruits)
+                {
+                    Console.WriteLine(fruit);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
